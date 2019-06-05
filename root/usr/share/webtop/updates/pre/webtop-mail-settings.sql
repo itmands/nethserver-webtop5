c319727a3985b5635f41dc8de275ece8a94a432a
Matteo Valentini <matteo.valentini@nethesis.it>
2019-06-05 16:45:45 +0200

    config: enable compact toolbar as default
    Only for new installations, for older installations will be preserved
    the previous default to "false"

--- root/usr/share/webtop/updates/pre/webtop-mail-settings.sql
+++ root/usr/share/webtop/updates/pre/webtop-mail-settings.sql
@@ -2,3 +2,8 @@
 -- Set Mail grid view to columns, if setting not exist
 -- ---------------------------------------------------
 INSERT INTO "core"."settings" (service_id, key, value) SELECT 'com.sonicle.webtop.mail', 'default.viewmode', 'columns' WHERE NOT EXISTS (SELECT value from "core"."settings" WHERE key = 'default.viewmode' and service_id = 'com.sonicle.webtop.mail');
+
+-- -------------------------------------------------------
+-- Set Mail compact toolbar to false, if setting not exist
+-- -------------------------------------------------------
+INSERT INTO "core"."settings" ("service_id", "key", "value") SELECT 'com.sonicle.webtop.mail', 'toolbar.compact', 'false'  WHERE NOT EXISTS (SELECT value from "core"."settings" WHERE key = 'toolbar.compact' and service_id = 'com.sonicle.webtop.mail');
