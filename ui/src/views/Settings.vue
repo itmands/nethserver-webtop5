e4f4ec392dc50be41dd908a946cd0a9a75cb4944
federicoballarini <fed.ballarini@gmail.com>
2019-12-20 02:19:03 +0100

    ui & api. update settings page

--- ui/src/views/Settings.vue
+++ ui/src/views/Settings.vue
@@ -13,7 +13,173 @@
 
     <div v-if="!uiLoaded" class="spinner spinner-lg"></div>
     <div v-if="uiLoaded">
-
+      <form class="form-horizontal" v-on:submit.prevent="saveConfig()">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="col-md-6">
+              <div :class="['form-group margintop', errors.DefaultLocale.hasError ? 'has-error' : '']">
+                <label class="col-sm-5 control-label">
+                  {{$t('settings.default_locale')}}
+                  <doc-info
+                    :placement="'top'"
+                    :title="$t('settings.default_locale')"
+                    :chapter="'DefaultLocale'"
+                    :inline="true"
+                  ></doc-info>
+                </label>
+                <div class="col-sm-5">
+                  <select
+                    required
+                    v-model="configuration.DefaultLocale"
+                    class="combobox form-control"
+                  >
+                    <option v-for="(t,i) in configuration.LocaleList" v-bind:key="i">{{t}}</option>
+                  </select>
+                  <span v-if="errors.DefaultLocale.hasError" class="help-block">{{$t('settings.not_valid_default_locale')}}</span>
+                </div>
+              </div>
+              <div :class="['form-group', errors.DefaultTimezone.hasError ? 'has-error' : '']">
+                <label class="col-sm-5 control-label">
+                  {{$t('settings.default_timezone')}}
+                  <doc-info
+                    :placement="'top'"
+                    :title="$t('settings.default_timezone')"
+                    :chapter="'DefaultTimezone'"
+                    :inline="true"
+                  ></doc-info>
+                </label>
+                <div class="col-sm-5">
+                  <select
+                    required
+                    v-model="configuration.DefaultTimezone"
+                    class="combobox form-control"
+                  >
+                    <option v-for="(t,i) in configuration.TimezonesList" v-bind:key="i">{{t}}</option>
+                  </select>
+                  <span v-if="errors.DefaultTimezone.hasError" class="help-block">{{$t('settings.not_valid_default_timezone')}}</span>
+                </div>
+              </div>
+              <div :class="['form-group', errors.DefaultToolbarIconsSize.hasError ? 'has-error' : '']">
+                <label class="col-sm-5 control-label">
+                  {{$t('settings.default_toolbar_icons_size')}}
+                  <doc-info
+                    :placement="'top'"
+                    :title="$t('settings.default_toolbar_icons_size')"
+                    :chapter="'DefaultToolbarIconsSize'"
+                    :inline="true"
+                  ></doc-info>
+                </label>
+                <div class="col-sm-5">
+                  <select
+                    required
+                    v-model="configuration.DefaultToolbarIconsSize"
+                    class="combobox form-control"
+                  >
+                    <option value="small">{{$t('settings.small')}}</option>
+                    <option value="medium">{{$t('settings.medium')}}</option>
+                    <option value="large">{{$t('settings.large')}}</option>
+                  </select>
+                  <span v-if="errors.DefaultToolbarIconsSize.hasError" class="help-block">{{$t('settings.not_valid_default_toolbar_icons_size')}}</span>
+                </div>
+              </div>
+              <div :class="['form-group', errors.SmtpAuth.hasError ? 'has-error' : '']">
+                <label class="col-sm-5 control-label">
+                  {{$t('settings.smtp_auth')}}
+                  <doc-info
+                    :placement="'top'"
+                    :title="$t('settings.smtp_auth')"
+                    :chapter="'SmtpAuth'"
+                    :inline="true"
+                  ></doc-info>
+                </label>
+                <div class="col-sm-5">
+                  <input
+                    v-model="configuration.SmtpAuth"
+                    type="checkbox"
+                    class="form-control"
+                    true-value="enabled"
+                    false-value="disabled"
+                  >
+                  <span v-if="errors.SmtpAuth.hasError" class="help-block">{{$t('settings.not_valid_smtp_auth')}}</span>
+                </div>
+              </div>
+              <div :class="['form-group', errors.SmtpStarttls.hasError ? 'has-error' : '']">
+                <label class="col-sm-5 control-label">
+                  {{$t('settings.smtp_starttls')}}
+                  <doc-info
+                    :placement="'top'"
+                    :title="$t('settings.smtp_starttls')"
+                    :chapter="'SmtpStarttls'"
+                    :inline="true"
+                  ></doc-info>
+                </label>
+                <div class="col-sm-5">
+                  <input
+                    v-model="configuration.SmtpStarttls"
+                    type="checkbox"
+                    class="form-control"
+                    true-value="enabled"
+                    false-value="disabled"
+                  >
+                  <span v-if="errors.SmtpStarttls.hasError" class="help-block">{{$t('settings.not_valid_smtp_starttls')}}</span>
+                </div>
+              </div>
+              <div :class="['form-group', errors.PublicUrl.hasError ? 'has-error' : '']">
+                <label class="col-sm-5 control-label">
+                  {{$t('settings.public_url')}}
+                  <doc-info
+                    :placement="'top'"
+                    :title="$t('settings.public_url')"
+                    :chapter="'PublicUrl'"
+                    :inline="true"
+                  ></doc-info>
+                </label>
+                <div class="col-sm-5">
+                  <input
+                    type="text"
+                    class="form-control"
+                    v-model="configuration.PublicUrl"
+                  >
+                  <span v-if="errors.PublicUrl.hasError" class="help-block">{{$t('settings.not_valid_public_url')}}</span>
+                </div>
+              </div>
+              <div :class="['form-group', errors.DavServerUrl.hasError ? 'has-error' : '']">
+                <label class="col-sm-5 control-label">
+                  {{$t('settings.dav_server_url')}}
+                  <doc-info
+                    :placement="'top'"
+                    :title="$t('settings.dav_server_url')"
+                    :chapter="'DavServerUrl'"
+                    :inline="true"
+                  ></doc-info>
+                </label>
+                <div class="col-sm-5">
+                  <input
+                    type="text"
+                    class="form-control"
+                    v-model="configuration.DavServerUrl"
+                  >
+                  <span v-if="errors.DavServerUrl.hasError" class="help-block">{{$t('settings.not_valid_dav_server_url')}}</span>
+                </div>
+              </div>
+              
+              <!-- save button -->
+              <div class="form-group">
+                <label class="col-sm-5 control-label">
+                </label>
+                <div class="col-sm-5">
+                  <button 
+                    class="btn btn-primary margintop" 
+                    type="submit"
+                  >
+                    {{$t('save')}}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   </div>
 </template>
@@ -22,12 +188,24 @@
 export default {
   name: "Settings",
   mounted() {
-    ////
+    this.getConfig()
   },
   data() {
     return {
       uiLoaded: false,
-      errorMessage: null
+      errorMessage: null,
+      configuration: {
+        DefaultLocale: 'en_US',
+        DefaultTimezone: 'Etc/UTC',
+        DefaultToolbarIconsSize: 'medium',
+        SmtpAuth: 'disabled',
+        SmtpStarttls: 'disabled',
+        PublicUrl: null,
+        DavServerUrl: null,
+        LocaleList: null,
+        TimezonesList: null
+      },
+      errors: this.initErrors()
     }
   },
   methods: {
@@ -37,10 +215,128 @@ export default {
     },
     closeErrorMessage() {
       this.errorMessage = null;
+    },
+    getConfig() {
+      var context = this;
+      context.uiLoaded = false;
+      nethserver.exec(
+        ["nethserver-webtop5/settings/read"],
+        { action: "configuration" },
+        null,
+        function(success) {
+          try {
+            context.configuration = JSON.parse(success).configuration;
+          } catch (e) {
+            console.error(e);
+          }
+          context.uiLoaded = true;
+        },
+        function(error) {
+          context.showErrorMessage(context.$i18n.t("settings.error_reading_webtop5_configuration"), error);
+        }
+      );
+    },
+    saveConfig() {
+      var context = this;
+      var settingsObj = {
+        action: "edit",
+        "configuration": {
+          DefaultLocale: context.configuration.DefaultLocale,
+          DefaultTimezone: context.configuration.DefaultTimezone,
+          DefaultToolbarIconsSize: context.configuration.DefaultToolbarIconsSize,
+          SmtpAuth: context.configuration.SmtpAuth,
+          SmtpStarttls: context.configuration.SmtpStarttls,
+          PublicUrl: context.configuration.PublicUrl,
+          DavServerUrl: context.configuration.DavServerUrl
+        }
+      };
+      context.loaders = true;
+      context.errors = context.initErrors();
+      nethserver.exec(
+        ["nethserver-webtop5/settings/validate"],
+        settingsObj,
+        null,
+        function(success) {
+          context.loaders = false;
+
+          // update values
+          nethserver.exec(
+            ["nethserver-webtop5/settings/update"],
+            settingsObj,
+            function(stream) {
+              console.info("nethserver-webtop5-update", stream);
+            },
+            function(success) {
+              context.getConfig();
+              nethserver.notifications.success = context.$i18n.t(
+                "settings.settings_updated_ok"
+              );
+            },
+            function(error, data) {
+              console.error(error, data);
+              nethserver.notifications.error = context.$i18n.t(
+                "settings.settings_updated_error"
+              );
+            },
+            true //sudo
+          );
+        },
+        function(error, data) {
+          var errorData = {};
+          context.loaders = false;
+          context.errors = context.initErrors();
+          try {
+            errorData = JSON.parse(data);
+            for (var e in errorData.attributes) {
+              var attr = errorData.attributes[e];
+              context.errors[attr.parameter].hasError = true;
+              context.errors[attr.parameter].message = attr.error;
+            }
+          } catch (e) {
+            console.error(e);
+          }
+        },
+        true // sudo
+      );
+    },
+    initErrors() {
+      return {
+        DefaultLocale: {
+          hasError: false,
+          message: ""
+        },
+        DefaultTimezone: {
+          hasError: false,
+          message: ""
+        },
+        DefaultToolbarIconsSize: {
+          hasError: false,
+          message: ""
+        },
+        SmtpAuth: {
+          hasError: false,
+          message: ""
+        },
+        SmtpStarttls: {
+          hasError: false,
+          message: ""
+        },
+        PublicUrl: {
+          hasError: false,
+          message: ""
+        },
+        DavServerUrl: {
+          hasError: false,
+          message: ""
+        }
+      }
     }
   }
 };
 </script>
 
 <style scoped>
+.margintop {
+  margin-top: 15px;
+}
 </style>
